5695cd23b91a6d8ef6122136c6a97fa9725ca647
dewandemo <61483131+dewandemo@users.noreply.github.com>
2023-10-18 15:11:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# Endpoint to echo a hello message with variable input
+@app.route('/hello/<name>', methods=['GET'])
+def hello(name):
+    return f'Hello, {name}!'
 
-@app.route('/')
-def hello():
-    return "Hello Tekton!"
+# Endpoint to show app health
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status': 'healthy'})
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(debug=True)
