9a63db08e375113ff5aa4eb1385cdb4a50a95763
dewandemo <61483131+dewandemo@users.noreply.github.com>
2024-03-11 16:04:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ value2 = random.randint(101, 200)
 
 # Write values to a file
 with open('/output/output.txt', 'w') as file:
-    file.write(f'VAR1={value1}\nVAR2={value2}')
+    file.write(f'VAR1={value1}\nVAR2={value2}\n')
 
 print(f'Values written to /output/output.txt: VAR1={value1}, VAR2={value2}')
